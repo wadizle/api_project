17434bdaf865c579b6a45130d2b9c28b28cc36f9
Brandon Waddill <bwadizle@gmail.com>
2020-10-05 13:52:30 -0400

    fixed curly

--- src/server.js
+++ src/server.js
@@ -16,41 +16,35 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 // };
 
 const onRequest = (request, response) => {
-    const parsedUrl = url.parse(request.url);
-    // const params = query.parse(parsedUrl.query);
-    // const requestType = request.headers.accept.split(',');
+  const parsedUrl = url.parse(request.url);
+  // const params = query.parse(parsedUrl.query);
+  // const requestType = request.headers.accept.split(',');
 
-    if (request.method === 'POST') {
-        const form = formidable.IncomingForm();
-        form.parse(request, (err, fields, files) => {
-            console.log("uploaded file")
-            const oldPath = files.filetoupload.path;
-            const newPath = path.join(__dirname, '../uploads/') + files.filetoupload.name;
-    
-            fs.rename(oldPath, newPath, (error) => {
-                if (error) console.log(error);
-                //ADD RESPONSE
-            });
-        });
-  } 
-  else if (parsedUrl.pathname === '/getFiles') {
-     const fileArr = [];
-     fs.readdir(path.join(__dirname, '../uploads'), (err, files) => {
-         if (err) {
-             console.log(err);
-             // ADD RESPONSE
-         }
-         for (let i = 0; i < files.length; i++) {
-             fileArr.push(files[i]);
-         }
-         responseHandler.sendFiles(request, response, fileArr);
-     });
-  } 
-  //else if (parsedUrl.pathname === '/downloadFile') {
+  if (request.method === 'POST') {
+    const form = formidable.IncomingForm();
+    form.parse(request, (err, fields, files) => {
+      console.log('uploaded file');
+      const oldPath = files.filetoupload.path;
+      const newPath = path.join(__dirname, '../uploads/') + files.filetoupload.name;
 
-
-  //} 
-  else if (parsedUrl.pathname === '/') {
+      fs.rename(oldPath, newPath, (error) => {
+        if (error) console.log(error);
+        // ADD RESPONSE
+      });
+    });
+  } else if (parsedUrl.pathname === '/getFiles') {
+    const fileArr = [];
+    fs.readdir(path.join(__dirname, '../uploads'), (err, files) => {
+      if (err) {
+        console.log(err);
+        // ADD RESPONSE
+      }
+      for (let i = 0; i < files.length; i++) {
+        fileArr.push(files[i]);
+      }
+      responseHandler.sendFiles(request, response, fileArr);
+    });
+  } else if (parsedUrl.pathname === '/') {
     htmlHandler.getIndex(request, response);
   } else if (parsedUrl.pathname === '/style.css') {
     htmlHandler.getCSS(request, response);
@@ -58,6 +52,10 @@ const onRequest = (request, response) => {
     // urlStruct.notFound(request, response);
     responseHandler.notFound(request, response);
   }
+
+  // else if (parsedUrl.pathname === '/downloadFile') {
+
+  // }
 
 //  if (request.method === 'POST') {
 //    const body = [];
